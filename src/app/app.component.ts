9c8a55afe993ace3d5b75d68340ece5f48f60466
Daprogam <aguilarpol99@gmail.com>
2022-09-02 20:02:42 +0200

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title: string = 'To Do List';
+  tasks: string[] = [];
+
+  updateTasks(newTask: string):void{
+    this.tasks.push(newTask);
+
+  }
+
+
 }
